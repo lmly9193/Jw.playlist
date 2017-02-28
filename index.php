be641afb9ecaf221b05a4d4eeee35906343c8852
Ethan <lmly9193@users.noreply.github.com>
2017-02-28 18:04:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAA3NCSVQICAjb4U/gAAAACXBIWXMAAABvAAAAbwHxotxDAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAFFQTFRF////AAAAGwAALQEBPgICOAICSCEhTwMDUkVFUkZGVD4+VQMDXx0dghERkgUFmQUFmo+PnpaWogUFpQUFrgYGuQYGugYG0wcH1QcH6Ojo////VIe8EAAAAAZ0Uk5TAAhV1OLk46qZIwAAAFJJREFUGFdjYMAEjEgAyGViZkECzEwMzBIogJmBFUwLiUMFWCECwhxsoigC/IKSXLyoAlJSAjz4BdC0CHOiGYpsLdxhfOzs3CJizJhOx/AcOgAA5zwM2aaHDyMAAAAASUVORK5CYII=">
     <script src='jwplayer/jwplayer.js'></script>
     <script src='jwplayer/provider.html5.js'></script>
-    <script src='https://lmly9193.github.io/json.playlist/key.js'></script>
+    <script src='https://lmly9193.github.io/Jw.playlist/key.js'></script>
     <link rel="stylesheet" href='jwplayer/skins/fullscreen.css'>
     <link rel="stylesheet" href='jwplayer/skins/tube.css'>
 </head>
