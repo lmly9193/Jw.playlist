0dfc416b5f5a16b1c81f671a1f25d5f135603f25
廖昱信 <lmly9193@gmail.com>
2017-02-28 19:40:38 +0800

    lessfull

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>TITLE</title>
+    <title>Video Embeding System</title>
     <meta name="description" content="DESCRIPTION">
-    <!--<meta name="viewport" content="width=device-width">-->
+    <meta name="viewport" content="width=device-width">
     <link rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAA3NCSVQICAjb4U/gAAAACXBIWXMAAABvAAAAbwHxotxDAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAFFQTFRF////AAAAGwAALQEBPgICOAICSCEhTwMDUkVFUkZGVD4+VQMDXx0dghERkgUFmQUFmo+PnpaWogUFpQUFrgYGuQYGugYG0wcH1QcH6Ojo////VIe8EAAAAAZ0Uk5TAAhV1OLk46qZIwAAAFJJREFUGFdjYMAEjEgAyGViZkECzEwMzBIogJmBFUwLiUMFWCECwhxsoigC/IKSXLyoAlJSAjz4BdC0CHOiGYpsLdxhfOzs3CJizJhOx/AcOgAA5zwM2aaHDyMAAAAASUVORK5CYII=">
     <script src='jwplayer/jwplayer.js'></script>
     <script src='jwplayer/provider.html5.js'></script>
     <script src='https://lmly9193.github.io/Jw.playlist/key.js'></script>
-    <link rel="stylesheet" href='/css/fullscreen.css'>
+    <link rel="stylesheet" href='css/fullscreen.css'>
 </head>
 
 <body>
@@ -44,7 +44,6 @@
 
             //Rendering and Loading
             primary: "html5", //default:"html5" or "flash"
-            flashplayer: "jwplayer/jwplayer.flash.swf",
             preload: "metadata", //"auto","none","metadata"
 
             //Skin
