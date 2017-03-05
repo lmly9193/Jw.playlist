4dce1b85c025b6e6e695743abaa88d5d7603c826
廖昱信 <lmly9193@gmail.com>
2017-03-05 20:50:21 +0800

    str_ireplace

--- script.php
+++ script.php
@@ -14,7 +14,7 @@
 				$value=json_decode(curl("https://api.getlinkdrive.com/getlink?url=".$value),true);
 				#remove "src","res" and re-value label
 				foreach(array_keys($value) as &$key){
-					$value[$key]["label"]=strtr($value[$key]["label"],"pP","  ");//str_ireplace("p","",$value[$key]["label"]);
+					$value[$key]["label"]=str_ireplace("p","",$value[$key]["label"]);
 					unset($value[$key]["src"],$value[$key]["res"]);
 				}
 				#ksort array
