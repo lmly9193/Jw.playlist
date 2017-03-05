857e63d87d8eca4cf08ce83624a43d13a1d3684a
廖昱信 <lmly9193@gmail.com>
2017-03-05 20:25:54 +0800

    function

--- playlist.php
+++ playlist.php
@@ -1,31 +1,35 @@
 <?php
 	ini_set("memory_limit","2048M");
-	if(empty($json_playlist=json_decode(curl($_GET["playlist"]),true))){
-		echo '["Error loading playlist"]';
-	}else{
-		#curl json.playlist to array $json_playlist
-		#fmt_stream_map for each
-		$fmt_stream_map=array_column($json_playlist,"sources");
-		foreach($fmt_stream_map as &$value){
-			preg_match("/(?:https?:\/\/)?(?:[\w\-]+\.)*(?:drive|docs)\.google\.com\/(?:(?:folderview|open|uc)\?(?:[\w\-\%]+=[\w\-\%]*&)*id=|(?:folder|file|document|presentation)\/d\/|spreadsheet\/ccc\?(?:[\w\-\%]+=[\w\-\%]*&)*key=)([\w\-]{28,})/i",$value,$match);
-			$value=json_decode(curl("https://api.getlinkdrive.com/getlink?url=".$value),true);
-			#remove "src","res" and re-value label
-			foreach(array_keys($value) as &$key){
-				$value[$key]["label"]=str_ireplace("p","",$value[$key]["label"]);
-				unset($value[$key]["src"],$value[$key]["res"]);
+	#playlist function
+	function playlist($playlistfile){
+		if(empty($json_playlist=json_decode(curl($playlistfile),true))){
+			echo '["Error loading playlist"]';
+		}else{
+			#curl json.playlist to array $json_playlist
+			#fmt_stream_map for each
+			$fmt_stream_map=array_column($json_playlist,"sources");
+			foreach($fmt_stream_map as &$value){
+				$value=urldecode($value);
+				preg_match("/(?:https?:\/\/)?(?:[\w\-]+\.)*(?:drive|docs)\.google\.com\/(?:(?:folderview|open|uc)\?(?:[\w\-\%]+=[\w\-\%]*&)*id=|(?:folder|file|document|presentation)\/d\/|spreadsheet\/ccc\?(?:[\w\-\%]+=[\w\-\%]*&)*key=)([\w\-]{28,})/i",$value,$match);
+				$value=json_decode(curl("https://api.getlinkdrive.com/getlink?url=".$value),true);
+				#remove "src","res" and re-value label
+				foreach(array_keys($value) as &$key){
+					$value[$key]["label"]=str_ireplace("p","",$value[$key]["label"]);
+					unset($value[$key]["src"],$value[$key]["res"]);
+				}
+				#ksort array
+				foreach($value as $label=>&$quality){
+					$ksort_array[$label]=$quality["label"];
+				}
+				array_multisort($ksort_array,SORT_DESC,$value);
+				#replace sources and add preview
+				$value=array(sources=>$value,fileId=>$match["1"]);
 			}
-			#ksort array
-			foreach($value as $label=>$quality){
-				$ksort_array[$label]=$quality["label"];
-			}
-			array_multisort($ksort_array,SORT_DESC,$value);
-			#replace sources and add preview
-			$value=array(sources=>$value,fileId=>$match["1"]);
+			#combine $fmt_stream_map into $json_playlist
+			$playlist=array_replace_recursive($json_playlist,$fmt_stream_map);
+			#print $playlist by json
+			echo json_encode($playlist,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
 		}
-		#combine $fmt_stream_map into $json_playlist
-		$playlist=array_replace_recursive($json_playlist,$fmt_stream_map);
-		#print $playlist by json
-		echo json_encode($playlist,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
 	}
 	#curl function
 	function curl($url){
@@ -48,4 +52,12 @@
 		curl_close($ch);
 		#delay 0.25 sec = 250000 
 		usleep(250000);
+	}
+	#urlhttps function
+	function urlhttps(){
+		if(isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"]=="on"){
+			 echo urldecode("https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
+		}else{
+			 echo urldecode("http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
+		}
 	}
