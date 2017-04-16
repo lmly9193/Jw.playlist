ebf6511949b55d86e027908e67ec1a72feec38a5
Ethan <lmly9193@gmail.com>
2017-04-16 19:52:00 +0800

    init

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-	foreach (getallheaders() as $name => $value){
-    echo "$name: $value"."<br>";
-	}
-	
-	
-	echo $_SERVER["REMOTE_ADDR"]."<br>";
-	echo $_SERVER["HTTP_CLIENT_IP"]."<br>";
-	echo $_SERVER["HTTP_X_FORWARDED_FOR"]."<br>";
